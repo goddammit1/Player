a7f66e9a11f8fc9b9c8a994ac0bd718dbf28ff40
goddammit1 <164378662+goddammit1@users.noreply.github.com>
2026-07-28 22:31:17 +0300

    fix: AppDelegate uses FlutterAppDelegate instead of AudioServiceActivity (not available in audio_service 0.18.x)

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,9 +1,8 @@
 import Flutter
 import UIKit
-import audio_service
 
 @main
-@objc class AppDelegate: AudioServiceActivity {
+@objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
